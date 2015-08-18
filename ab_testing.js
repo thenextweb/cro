6cb1c3a5a83553e34fdb7f8f92a7d28847f02e4c
Martijn Scheijbeler <martijn.scheijbeler@gmail.com>
2015-08-18 16:09:13 +0200

    Rewrite some lines to follow the JS style guide

--- ab_testing.js
+++ ab_testing.js
@@ -1,24 +1,24 @@
 // Variables
-var testID = '001',
-    testDays = 8,
-    randomNumber = {{Randon Number}},
-    testVariant = readCookie(testID),
-    variants = {
-      1: {
-        execute: function() {
-          //
+const testID = '001',
+      testDays = 8,
+      randomNumber = {{Randon Number}},
+      testVariant = readCookie(testID),
+      variants = {
+        1: {
+          execute: function() {
+            //
+          }
+        },
+        2: {
+          execute: function() {
+            //
+          }
         }
-      },
-      2: {
-        execute: function() {
-          //
-        }
-      }
-    };
+      };
 
 // Helpers
 function setCookie(testID, variantID, testDays) {
-  if (typeof(testDays)==='undefined') testDays = 8;
+  if (typeof(testDays) === 'undefined') testDays = 8;
   var date = new Date();
   date.setTime(date.getTime() + (testDays * 86400000));
   var expires = '; expires=' + date.toGMTString();
@@ -39,10 +39,11 @@ function readCookie(testID) {
 
 function inTest() {
   ca = document.cookie.split(';')
+
   for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf("tnw") > -1) return true;
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf("tnw-") > -1) return true;
   }
   return false;
 }
@@ -58,21 +59,21 @@ function control(testVariant) {
 
 function variant(variantID) {
   control(variantID);
-  if(variantID > 0) variants[variantID].execute();
+  if (variantID > 0) variants[variantID].execute();
 }
 
 // Check variants v.s. cookies
 if(testVariant) {
   for (var i = 0; i <= Object.keys(variants).length; i++) {
-    if(testVariant == testID + '-' + i) variant(i);
+    if (testVariant === testID + '-' + i) variant(i);
   }
 } else if(!inTest()) {
   var chosen = 0;
   for (var j = 0; j <= 2147483647; j+= Math.round(2147483647 / (Object.keys(variants).length + 1))) {
-    if(randomNumber <= Math.round(2147483647 / (Object.keys(variants).length) + 1)) {
+    if (randomNumber <= Math.round(2147483647 / (Object.keys(variants).length) + 1)) {
       variant(chosen);
       break;
-    } else if(randomNumber >= j && randomNumber < j + Math.round(2147483647 / (Object.keys(variants).length + 1)) ) {
+    } else if (randomNumber >= j && randomNumber < j + Math.round(2147483647 / (Object.keys(variants).length + 1))) {
       variant(chosen);
       break;
     }
